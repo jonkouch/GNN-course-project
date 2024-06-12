33d1f91a95f450bfeaf088202c5643a9f546ca59
Ben Eliav <ben.eliav@iegpu06.iem.technion.ac.il>
2024-06-13 01:13:33 +0300

    fixfix

--- utils/load_configs.py
+++ utils/load_configs.py
@@ -58,9 +58,9 @@ def get_link_prediction_args(args=None):
     parser.add_argument('--test_laser_snapshots', type=int, default=0, help='How many iterations of our dynamic LASER we want to do')
 
 
-    try:
-        args = parser.parse_args(args=args)
-        args.device = f'cuda:{args.gpu}' if torch.cuda.is_available() and args.gpu >= 0 else 'cpu'
+    # try:
+    args = parser.parse_args(args=args)
+    args.device = f'cuda:{args.gpu}' if torch.cuda.is_available() and args.gpu >= 0 else 'cpu'
         # args.device = 'cpu'
     # except Exception as e:
     #     parser.print_help()
